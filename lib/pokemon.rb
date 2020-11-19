5ff970903f9128737689277573658fd59548984d
Garrett <Garrett.Bodley@gmail.com>
2020-11-19 14:53:31 -0500

    commit tree 74f1ad9168f430d0ed00ce67a12c74f0d65a480c

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,28 @@
 class Pokemon
+  attr_accessor :name, :type
+  attr_reader :id, :db
+
+  def initialize(name:, type:, db:, id:)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+  end
+
+  def self.save(name, type, db)
+    sql = "INSERT INTO pokemon (name, type) VALUES (?, ?);"
+    db.execute(sql, name, type)
+  end
+
+  def self.update
+    sql = "UPDATE pokemon SET name = ?, type = ? WHERE id = ?;"
+    db.execute(sql, self.name, self.type, self.id)
+  end
+
+  def self.find(id, db)
+    sql = "SELECT * FROM pokemon WHERE id = ?;"
+    info = db.execute(sql, id).first
+    self.new(id: info[0], name: info[1], type: info[2], db: db)
+  end
+
 end
